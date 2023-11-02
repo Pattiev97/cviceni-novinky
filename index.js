e3f7ca8270c72ef1426d036b61e9136e11912023
Patricie Vyhlídalová <patina.vy@gmail.com>
2023-11-02 19:42:04 +0100

    2

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const bodyElement = document.querySelector("body")
-const news = document.querySelector("news")
+const news = document.querySelector(".news")
 const nadpis = document.querySelector("h1")
 const zprava1 = document.querySelector("#zprava1")
 const obrazek3 = document.querySelector("#zprava3")
 
-body.style.backgroundColor = "#e9e9e9"
-body.style.backgroundColor = "white"
-body.style.maxWidth = "60rem"
+bodyElement.style.backgroundColor = "#e9e9e9"
+news.style.backgroundColor = "white"
+news.style.maxWidth = "60rem"
 nadpis.classList.add("news__title")
 nadpis.textContent = "Aktuální novinky"
 zprava1.classList.add("post--main")
